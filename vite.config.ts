ca3dc2e29694c0abdeddd7cc4fd33f0d27f652be
Bongyh0714 <bongyhmj@gmail.com>
2026-02-02 23:45:42 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // 1. Allow access from network (for testing)
   server: {
     host: '0.0.0.0',
   },
-  // 2. Tell the robot exactly which files to keep
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',           // The Projector
-        controller: 'controller.html' // The Player App
+        main: 'index.html',           
+        controller: 'controller.html' 
       },
     },
   },
